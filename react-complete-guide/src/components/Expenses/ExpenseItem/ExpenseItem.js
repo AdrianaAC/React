b7868ef8e340e7adfab9040efe04ba7d42d4235f
Adriana <adrianaalves098@gmail.com>
2023-01-22 23:30:51 +0000

    Components org

--- react-complete-guide/src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,5 +1,5 @@
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
-import Card from "../Card";
+import Card from "../../Utils/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
