adcfb3a97482b27c289e6e789edc1cc0affc1fbb
Adriana <adrianaalves098@gmail.com>
2023-01-25 01:01:48 +0000

    individual state form

--- react-complete-guide/src/components/Input/Form/ExpenseForm.js
+++ react-complete-guide/src/components/Input/Form/ExpenseForm.js
@@ -1,19 +1,42 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
+
 const ExpenseForm = (props) => {
+  const [enteredDesc, setEnteredDesc] = useState(" ");
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState(" ");
+
+  const descChangeHandler = (event) => {
+    setEnteredDesc(event.target.value);
+  };
+
+  const ammountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Expense description</label>
-          <input type="text" />
+          <input type="text" onChange={descChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Expense ammount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={ammountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Expense date</label>
-          <input type="date" min="2010-01-01" max="2050-12-31" />
+          <input type="date" min="2010-01-01" max="2050-12-31" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
