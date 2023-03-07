00176d2b518529b422fb6be8312348d1135c0590
Adriana <adrianaalves098@gmail.com>
2023-03-07 00:41:47 +0000

    Remove inline css

--- react-complete-guide/C6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-complete-guide/C6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,18 +25,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "salmon" : " #2835a8" }}>
-          Course Goal
-        </label>
-        <input
-          style={{
-            borderColor: !isValid ? "salmon" : "#2835a8",
-            background: !isValid ? "salmon" : "transparent",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!isValid && "invalid"}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
